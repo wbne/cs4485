bf4637189233e0ebc5f85dba75f4bc8fa7e8d6b0
preesha01 <preesha.shankar@gmail.com>
2023-10-12 10:19:25 -0500

    Added homePage folder to components

---
+++ frontend/src/components/homePage/Header.tsx
@@ -0,0 +1,42 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import "@fontsource/playfair-display";
+import "@fontsource/inter";
+
+export default function ResponsiveAppBar() {
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl" sx={{color: 'black',backgroundColor: '#D9F4EF'}}>
+        <Toolbar disableGutters>
+			<Box display='flex' flexGrow={1}>
+				<Typography
+					variant="h6"
+					noWrap
+					component="a"
+					href="#app-bar-with-responsive-menu"
+					sx={{
+					ml: 5,
+					display: { xs: 'none', md: 'flex' }
+					}}
+				>
+					<Typography variant='h4' color='inherit' fontFamily='Playfair Display' fontWeight={600}>
+						Tutorkesh
+					</Typography>
+				</Typography>
+			</Box>
+			<Box>
+            {['Find Tutor', 'Favorites', 'Appointments', 'Profile'].map(text => (
+              <Button key={text} sx={{ textTransform: 'none', marginRight: 5 }}>
+                <Typography color='black' fontFamily='Inter'>{text}</Typography>
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
---
+++ frontend/src/components/homePage/HomePageBody.tsx
@@ -0,0 +1,70 @@
+import {Typography, Grid, Box } from '@mui/material';
+
+import "@fontsource/playfair-display";
+import "@fontsource/inter";
+import '@fontsource/inter';
+import '@fontsource/inter/300.css';
+
+// Define the data for each box
+const boxesData = [
+	{ number: 34, text: "hours spent on learning" },
+	{ number: 56, text: "days from your first day" },
+	{ number: 78, text: "sessions overall since you started" }
+  ];
+
+export default function LandingPageBody() {
+	
+  return (
+	
+		<div style={{ backgroundColor: 'white'}}>
+			<Box 
+				display="flex" 
+				flexDirection="column" 
+				justifyContent="center"  
+				height="30vh" 
+				alignItems="center"
+			>
+   				<Typography fontFamily='Playfair Display' fontSize={80} fontWeight='medium'>Welcome, Username!</Typography>
+    			<Typography fontFamily='Inter' fontSize={30} fontWeight='normal'>Continue your learning through tutoring</Typography>
+			</Box>
+			
+			
+			<Grid
+				container
+				direction="row"
+				alignItems="center"
+				justifyContent="space-around"
+				sx={{
+					height: '60vh',
+					width: '80%',
+					margin: '0 auto'
+				}}
+			>
+				{boxesData.map((box, index) => (
+					<Grid 
+						item 
+						//xs={3} 
+						key={index}
+						sx={{
+							backgroundColor: '#D9D9D95E',
+							borderRadius: '10px',
+							height: '50vh',
+							width: '22vw',
+							display: 'flex',
+							flexDirection: 'column',
+							justifyContent: 'center',
+							alignItems: 'center',
+							padding: '1rem',
+							gap: '1rem'
+						}}
+					>
+						<Typography fontFamily='Inter' fontSize={130} fontWeight='medium' color='#7D729E'>{box.number}</Typography>
+						<Typography fontFamily='Inter' fontSize={30} fontWeight='light'align='center'>{box.text}</Typography>
+					</Grid>
+				))}	
+				
+			</Grid>
+		</div>
+  );
+}
+
