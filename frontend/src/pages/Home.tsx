ec375cf65e1c33a2038f2a2c29b07783fd599058
preesha01 <preesha.shankar@gmail.com>
2023-10-12 10:24:55 -0500

    Added home page

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/homePage/Header";
+import HomePageBody from "../components/homePage/HomePageBody";
+ 
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <HomePageBody />
+        </div>
+    );
+};
+ 
+export default Home;
