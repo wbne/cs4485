6a0912f2b341b9d0dd8dd025839af6d6a40aa30d
preesha01 <preesha.shankar@gmail.com>
2023-10-20 12:01:19 -0500

    added tutor profile page

---
+++ frontend/src/pages/tutorProfilePage.tsx
@@ -0,0 +1,13 @@
+import Header from '../components/tutorProfilePage/Header';
+import TutorProfilePage from '../components/tutorProfilePage/tutorProfilePage';
+
+function TutorProfile() {
+  return (
+    <div style={{height: '100vh'}}>
+      <Header/>
+      <TutorProfilePage/>
+    </div>
+  );
+}
+
+export default TutorProfile;
