373de1fca1e102658c8dc007a0bfdd7d23175d83
preesha01 <preesha.shankar@gmail.com>
2023-11-03 17:09:01 -0500

    Design Changes

--- frontend/src/components/favoriteTutorPage/FavoriteTutor.tsx
+++ frontend/src/components/favoriteTutorPage/FavoriteTutor.tsx
@@ -22,31 +22,31 @@ export default function FavoriteTutorPage() {
         <div className='flex flex-row' style={{height: '90vh', width: '100vw',}}>
             <div style={{paddingLeft: '4rem', paddingTop: '2rem'}}>
                 <Box>
-                    <Typography  fontFamily='playfair-display' fontWeight={'medium'} fontSize={40}>Favorite Tutors</Typography>
+                    <Typography fontFamily='Playfair Display' fontSize={40} fontWeight='medium'>Favorite Tutors</Typography>
                 </Box>
                 <div className='flex flex-col justify-center'>
                     {tutors.map((tutor: Tutors, index: number) => {
                        
                             return (
-                                <Box key={index} sx={{width: '80vw', mt: 5, background: 'rgba(217, 217, 217, 0.37)'}}>
+                                <Box key={index} sx={{width: '80vw', mt: 5, background: 'rgba(217, 217, 217, 0.37)', borderRadius: 4}}>
                                     <div className='flex flex-row justify-between mt-3 mb-3'>
                                         <div className='flex flex-row pl-2'>
                                             <Avatar sx={{width: 56, height: 56, mt: 1}} alt={tutor.name} src={pfp} />
                                             <div className='pl-3 flex flex-col'>
-                                                <Typography fontSize={20} fontWeight='bold' fontFamily={'Inter'}>{tutor.name}</Typography>
-                                                <Typography fontSize={15} fontFamily={'Inter'}>{tutor.topic}</Typography>
-                                                <Typography fontSize={15} fontFamily={'Inter'}>More Information</Typography>
+                                                <Typography fontSize={20} fontWeight='bold' fontFamily={'Inter'} sx={{marginBottom: 1}}>{tutor.name}</Typography>
+                                                <Typography fontSize={15} fontWeight='normal' fontFamily={'Inter'} sx={{marginBottom: 1}}><span style={{ backgroundColor: '#D9D9D9', padding: 5, borderRadius: 5 }}>{tutor.topic}</span></Typography>
+                                                <Typography fontSize={15} fontWeight='lighter' fontFamily={'Inter'}>More Information</Typography>
                                             </div>
                                         </div>
                                         <div className='pr-5'>
                                             <Button sx={{backgroundColor: '#A6CAA9', color: 'black', mr:2, height: '30px'}}>
-                                                <Typography textTransform='none'>Book</Typography>
+                                                <Typography fontFamily='Inter' textTransform='none'>Book</Typography>
                                             </Button>
                                             <Button sx={{backgroundColor: '#F0B8B8', color: 'black', height: '30px', '&:hover': {
-                                                backgroundColor: '#000',
-                                                color: '#F0B8B8'
+                                                backgroundColor: 'white',
+                                                color: '#black'
                                             }}}>
-                                                <Typography textTransform='none'>Delete</Typography>
+                                                <Typography fontFamily='Inter' textTransform='none'>Delete</Typography>
                                             </Button>
                                         </div>
                                     </div>
