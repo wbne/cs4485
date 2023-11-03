18e10cf0ea8571156ba26a73dd0296975ca8b367
preesha01 <preesha.shankar@gmail.com>
2023-11-03 15:22:13 -0500

    Design Changes

--- frontend/src/components/appointmentPage/AppointmentPage.tsx
+++ frontend/src/components/appointmentPage/AppointmentPage.tsx
@@ -8,7 +8,7 @@ import '@fontsource/inter';
 import '@fontsource/inter/300.css';
 
 const AppointmentsBox = styled(Box)(({ theme }) => ({
-    backgroundColor: '#D9D9D9',
+    backgroundColor: 'rgba(217, 217, 217, 0.5)',
     padding: theme.spacing(2),
     marginBottom: theme.spacing(2),
     display: 'flex',
@@ -21,15 +21,22 @@ const AppointmentsBox = styled(Box)(({ theme }) => ({
 const AppointmentsPage = () => {
     return (
         <div className='flex' style={{height: '90vh', width: '100vw',}}>
-            <Box padding={4}>   
+            <Box padding={2}>   
                 <Box padding={2}>
                     <Typography fontFamily='Playfair Display' sx={{fontSize:{xs:40, md:60}, mb: 2}} fontWeight='medium'>Appointments</Typography>
-                    <Button href={'/appointments/book'}sx={{backgroundColor: '#7D729E', color: 'white', mr:4, height: '40px', textTransform: 'none'}}>
+                    <Button href={'/appointments/book'}sx={{
+                        backgroundColor: '#7D729E', 
+                        color: 'white', 
+                        mr:4, 
+                        height: '40px', 
+                        boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+                        textTransform: 'none', '&:hover': { backgroundColor: 'white', color: '#7D729E'}
+                    }}>
                         <Typography fontFamily='Inter' fontWeight='light'>Book New Appointment</Typography>
                     </Button>
                 </Box>
 
-                <Box padding={2}>
+                <Box padding={2} marginTop={2}>
                     <Typography fontFamily='Inter' fontWeight='bold' sx={{fontSize:{xs:15, md:20}, mb: 2}}>View Upcoming Tutoring Appointments</Typography>
                     
                     <Grid container spacing={2} style={{display: 'flex'}}>
@@ -40,14 +47,29 @@ const AppointmentsPage = () => {
                                         <Typography fontFamily='Inter' fontWeight='normal' color={'#7D729E'}>{appointment.date}</Typography>
                                         <Typography fontFamily='Inter' fontWeight='normal' color={'#7D729E'} sx={{mb: 2}}>{appointment.time}</Typography>
                                         <Typography fontFamily='Inter' fontWeight= 'bold' color= 'black'>{appointment.tutorName}</Typography>
-                                        <Typography fontFamily='Inter' fontWeight= 'normal' color= 'black'>{appointment.topic}</Typography>
-                                        <Typography fontFamily='Inter' fontWeight="lighter" sx={{fontSize: '13px'}}>{appointment.notes}</Typography>
+                                        <Typography fontFamily='Inter' fontWeight= 'normal' sx={{color: 'black', marginTop: 1}}><span style={{backgroundColor: '#D9D9D9', padding: 5, borderRadius: 5}}>{appointment.topic}</span></Typography>
+                                        <Typography fontFamily='Inter' fontWeight="lighter" sx={{fontSize: '13px', marginTop: 1}}>{appointment.notes}</Typography>
                                     </Box>
                                     <Box display="flex" justifyContent="center" marginTop={2}>
-                                        <Button variant="contained" sx={{backgroundColor: "#A5CAA9", color: 'black', mr: 2}}>Meet</Button>
-                                        <Button variant="contained" sx={{backgroundColor: "#F0B8B8", color: 'black', ml: 2}}>Cancel</Button>
-                                        
-                                    </Box>
+    <Button variant="contained" sx={{
+        backgroundColor: "#A5CAA9",
+        color: 'black',
+        mr: 2,
+        boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)', // Add permanent drop shadow
+        '&:hover': {
+            backgroundColor: 'rgba(165, 202, 169, 0.3)',
+        }
+    }}>Meet</Button>
+    <Button variant="contained" sx={{
+        backgroundColor: "#F0B8B8",
+        color: 'black',
+        ml: 2,
+        boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)', // Add permanent drop shadow
+        '&:hover': {
+            backgroundColor: 'rgba(240, 184, 184, 0.3)',
+        }
+    }}>Cancel</Button>
+</Box>
                                 </AppointmentsBox>
                             </Grid>
                         ))}
