e29ed0ce05da7616cbd46412be9d681294ab4930
preesha01 <preesha.shankar@gmail.com>
2023-11-22 15:28:46 -0600

    API field match + change password

--- frontend/src/components/tutorProfilePage/tutorProfilePage.tsx
+++ frontend/src/components/tutorProfilePage/tutorProfilePage.tsx
@@ -1,76 +1,114 @@
-import { Box, Typography, Button, TextField } from '@mui/material';
+import { Box, Typography, Button, TextField, Dialog, DialogContent, DialogActions } from '@mui/material';
 import "@fontsource/playfair-display";
 import "@fontsource/inter";
 import '@fontsource/inter';
 import '@fontsource/inter/300.css';
 import { useState } from 'react';
-import dayjs, { Dayjs } from 'dayjs';
-import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
-import { DatePicker } from '@mui/x-date-pickers';
 
 export default function TutorProfilePage() {
-    const [value, setValue] = useState<Dayjs | null>(dayjs(new Date()));
-    const MAX_AGE = 100;
-    const MIN_AGE = 18;
     const [teachingClearanceStatus, setTeachingClearanceStatus] = useState('Approved');
+    const [password, setPassword] = useState('');
+    const [showPasswordBtn, setShowPasswordBtn] = useState(true);
+    const [showPasswordFields, setShowPasswordFields] = useState(false);
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [openDialog, setOpenDialog] = useState(false);
+    const [passwordsMatch, setPasswordsMatch] = useState(false);
+    const [passwordStrength, setPasswordStrength] = useState({
+        length: false,
+        uppercase: false,
+        lowercase: false,
+        number: false,
+        specialChar: false,
+      });
+    const passwordIsStrong =
+        passwordStrength.length &&
+        passwordStrength.uppercase &&
+        passwordStrength.lowercase &&
+        passwordStrength.number &&
+        passwordStrength.specialChar;
+    
+    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newPassword = event.target.value;
+        setPassword(newPassword);
+        
+        if (newPassword === '' || confirmPassword === '') {
+            setPasswordsMatch(false);
+        } else {
+            setPasswordsMatch(newPassword === confirmPassword);
+        }
+        
+        const strength = {
+            length: newPassword.length >= 8,
+            uppercase: /[A-Z]/.test(newPassword),
+            lowercase: /[a-z]/.test(newPassword),
+            number: /\d/.test(newPassword),
+            specialChar: /[!@#$%^&*(),.?":{}|<>]/.test(newPassword),
+        };
+        setPasswordStrength(strength);
+    };
+
+    const handleConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newConfirmPassword = event.target.value;
+        setConfirmPassword(newConfirmPassword);
+        
+        if (password === '' || newConfirmPassword === '') {
+            setPasswordsMatch(false); 
+        } else {
+            setPasswordsMatch(newConfirmPassword === password);
+        }
+
+    };
+
+    const handleShowPasswordFields = () => {
+        setShowPasswordFields(true);
+        setShowPasswordBtn(false);
+    };
+
+    const handleSetNewPassword = () => {
+        if (password !== '' && confirmPassword !== '' && password === confirmPassword) {
+            setOpenDialog(true);
+            setPassword('');
+            setConfirmPassword('');
+            setPasswordsMatch(false)
+            setShowPasswordFields(false);
+            setShowPasswordBtn(true);
+            
+        } else {
+            alert("Passwords don't match. Please try again.");
+        }
+    };
+
+    const handleCancelPasswordChange = () => {
+        setPassword('');
+        setConfirmPassword('');
+        setPasswordsMatch(false)
+        setShowPasswordFields(false);
+        setShowPasswordBtn(true);
+    };
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+    };
 
 
     return (
         <div className='flex flex-row' style={{height: '100vh', width: '100vw',}}>
-            <div style={{backgroundColor: 'rgba(217, 217, 217, 0.37)', width:'40%', paddingLeft: '4rem', paddingTop: '4rem', height: '100%'}}>
+            
+            <div style={{backgroundColor: 'rgba(217, 217, 217, 0.37)', width:'40%', paddingLeft: '4rem', paddingTop: '6rem', height: '100%'}}>
                 <Box>
                     <Typography fontFamily='Playfair Display' fontSize={60} fontWeight='medium'>Profile</Typography>
                     <Typography fontFamily='Inter' fontSize={20} fontWeight={'regular'}>Tutor Account</Typography>
                 </Box>
             </div>
-            <div className='flex flex-row justify-center' style={{height: '80%', width: '70%', paddingTop: '4rem'}}>
+
+            <div className='flex flex-row justify-center' style={{height: '80%', width: '70%', paddingTop: '5rem'}}>
                 <div className='flex flex-col justify-around w-7/12'>
-                    <TextField
-                    required
-                    id="firstName"
-                    label="First Name"
-                    sx={{width: '100%', marginBottom: '1rem'}}
-                    variant='filled'
-                    />
-                    <TextField
-                    required
-                    id="lastName"
-                    label="Last Name"
-                    sx={{width: '100%', marginBottom: '1rem'}}
-                    variant='filled'
-                    />
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DatePicker
-                        label="Date of birth"
-                        value={value}
-                        onChange={(newValue) => setValue(newValue)}
-                        slotProps={{
-                            textField: {
-                                variant: "filled",
-                                required: true,
-                            },
-                        }}
-                        disableFuture
-                        
-                        />
-                    </LocalizationProvider>
-                    <TextField
-                    required
-                    id="user"
-                    label="Username"
-                    sx={{width: '100%', marginBottom: '1rem', marginTop: '1rem'}}
-                    variant='filled'
-                    />
-                    <TextField
-                    required
-                    id="pwd"
-                    label="Password"
-                    sx={{width: '100%', marginBottom: '1rem'}}
-                    variant='filled'
-                    />
+
+                    <Typography fontFamily='Inter' fontSize={20} fontWeight={'regular'}>FirstName</Typography>
+                    <Typography fontFamily='Inter' fontSize={20} fontWeight={'regular'}>LastName</Typography>
+                    <Typography fontFamily='Inter' fontSize={20} fontWeight={'regular'}>username.email@given.com</Typography>
+
+                    {/* Label should read the tutor about me filled in registration */}
                     <TextField
                     required
                     id="abt"
@@ -78,26 +116,90 @@ export default function TutorProfilePage() {
                     sx={{width: '100%'}}
                     variant='filled'
                     />
-                    <Typography fontFamily='Inter' sx={{ width: '100%', color: '#7E729F', fontWeight: 'Light', marginBottom: '2rem', marginTop: '1rem' }}>
+
+                    <Typography fontFamily='Inter' sx={{ width: '100%', color: '#7E729F', fontWeight: 'Light' }}>
                         Teaching Clearance: {teachingClearanceStatus}
                     </Typography>
-                    <div className='flex flex-row w-full'>
-                        <Button sx={{color: 'black', borderColor: '#7E729F', '&:hover': {
-                                color: '#fff',
-                                backgroundColor: '#7E729F',
-                                border: 'none'
-                        }}} variant="outlined">
-                            <Typography fontFamily='Inter' textTransform='none'>Cancel</Typography>
-                        </Button>
-                        <Button sx={{backgroundColor: '#7E729F', ml: 2, '&:hover': {
-                                backgroundColor: '#fff',
-                                color: '#7E729F',
-                        }}} variant="contained">
-                            <Typography fontFamily='Inter' textTransform='none'>Save Changes</Typography>
-                        </Button>
+
+                    <div style={{width: '60%'}}>
+                        {/* Button to show password fields */}
+                        {showPasswordBtn && (
+                            <>
+                                <Button
+                                    sx={{ backgroundColor: '#7E729F', '&:hover': { backgroundColor: '#fff', color: '#7E729F', } }}
+                                    variant="contained"
+                                    onClick={handleShowPasswordFields}
+                                >
+                                    <Typography fontFamily='Inter' textTransform='none'>Change Password</Typography>
+                                </Button>
+                            </>
+                        )}
+
+                        {/* Conditional rendering of password fields */}
+                        {showPasswordFields && (
+                            <>
+                                <TextField
+                                    required
+                                    id="pwd"
+                                    variant='filled'
+                                    label="Password"
+                                    type='password'
+                                    sx={{ width: '100%', mt: 2 }}
+                                    value={password}
+                                    onChange={handlePasswordChange}
+                                />
+                                <Typography variant="body2" 
+                                    sx={{ marginTop: 1, color: passwordIsStrong ? '#A6CAA9' : '#D32F2F' }}>
+                                    Password Strength: 
+                                        {passwordIsStrong ? ' Strong' : ' Weak' }
+                                </Typography>
+                                <TextField
+                                    required
+                                    id="confirmPwd"
+                                    variant='filled'
+                                    label="Confirm Password"
+                                    type='password'
+                                    sx={{ width: '100%', mt: 2 }}
+                                    value={confirmPassword}
+                                    onChange={handleConfirmPasswordChange}
+                                    error={!passwordsMatch} 
+                                    helperText={!passwordsMatch && 'Passwords do not match'}
+                                />
+                                <Button
+                                    sx={{ backgroundColor: '#A6CAA9', mt: 2 }}
+                                    disabled={!passwordsMatch || !passwordIsStrong}
+                                    variant="contained"
+                                    onClick={handleSetNewPassword}
+                                >
+                                    <Typography fontFamily='Inter' textTransform='none'>Set New Password</Typography>
+                                </Button>
+                                <Button
+                                    sx={{ backgroundColor: '#000', color: '#fff', mt: 2, ml: 2}}
+                                    variant="contained"
+                                    onClick={handleCancelPasswordChange}
+                                >
+                                    <Typography fontFamily='Inter' textTransform='none'>Cancel</Typography>
+                                </Button>
+                            </>
+                        )}
                     </div>
+                    
                 </div>
+                
+                {/* Dialog for displaying a success message */}
+                <Dialog open={openDialog} onClose={handleCloseDialog}>
+                    <DialogContent>
+                        <Typography>Password changed successfully!</Typography>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleCloseDialog} color="primary">
+                            Close
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+
             </div>
+
         </div>
     );
 }
